689647ecd7cfc7ca3373eed5ab8de2c4bb095512
Zainab-stack <zainabjamal350@gmail.com>
2024-09-29 14:41:59 +0500

    Add files via upload

---
+++ traffic light signal 2.py
@@ -0,0 +1,8 @@
+zstraffic=str(input("enter your color red,green,yellow"))
+match traffic:
+    case "red":
+        print ("stop")
+    case "yellow":
+        print ("ready")
+    case "green":
+        print ("go")
